9a8313aa8a67617a4a87fa6198bac85175325cbc
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-06-13 15:05:03 +0700

    Update addv2ray.sh

--- addv2ray.sh
+++ addv2ray.sh
@@ -41,61 +41,6 @@ until [[ $1 =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 			exit 1
 		fi
 	done
-uuid=$(cat /proc/sys/kernel/random/uuid)
-cat>/etc/xray/vmess-$1-tls.json<<EOF
-      {
-      "v": "2",
-      "ps": "$1",
-      "add": "${domain}",
-      "port": "${tls}",
-      "id": "${uuid}",
-      "aid": "0",
-      "net": "ws",
-      "path": "/jrtunnel",
-      "type": "none",
-      "host": "",
-      "tls": "tls"
-}
-EOF
-cat>/etc/xray/vmess-$1-nontls.json<<EOF
-      {
-      "v": "2",
-      "ps": "$1",
-      "add": "${domain}",
-      "port": "${nontls}",
-      "id": "${uuid}",
-      "aid": "0",
-      "net": "ws",
-      "path": "/jrtunnel",
-      "type": "none",
-      "host": "",
-      "tls": "none"
-}
-EOF
-vmess_base641=$( base64 -w 0 <<< $vmess_json1)
-vmess_base642=$( base64 -w 0 <<< $vmess_json2)
-xrayv2ray1="vmess://$(base64 -w 0 /etc/xray/vmess-$1-tls.json)"
-xrayv2ray2="vmess://$(base64 -w 0 /etc/xray/vmess-$1-nontls.json)"
 systemctl restart xray.service
 service cron restart
 clear
-echo -e ""
-echo -e "======-XRAYS/VMESS-======"
-echo -e "Remarks     : $1"
-echo -e "IP/Host     : ${MYIP}"
-echo -e "Address     : ${domain}"
-echo -e "Port TLS    : ${tls}"
-echo -e "Port No TLS : ${nontls}"
-echo -e "User ID     : ${uuid}"
-echo -e "Alter ID    : 0"
-echo -e "Security    : auto"
-echo -e "Network     : ws"
-echo -e "Path        : /jrtunnel"
-echo -e "Created     : $hariini"
-echo -e "Expired     : $exp"
-echo -e "========================="
-echo -e "Link TLS    : ${xrayv2ray1}"
-echo -e "========================="
-echo -e "Link No TLS : ${xrayv2ray2}"
-echo -e "========================="
-echo -e "Script Mod By VStunnel"
